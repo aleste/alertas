03323813aa66c0416b2f1b078aed519fea1d233e
Alejandro Steinmetz <asteinmetz78@gmail.com>
2015-08-15 17:20:52 -0300

    Change AlertDialog

--- app/src/main/java/com/aleste/alertas/MainActivity.java
+++ app/src/main/java/com/aleste/alertas/MainActivity.java
@@ -57,25 +57,29 @@ public class MainActivity extends AppCompatActivity {
      * */
     public void showAlertDialog(Context context, String title, String message, Boolean status) {
 
-        AlertDialog alertDialog = new AlertDialog.Builder(context).create();
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
 
-        // Setting Dialog Title
-        alertDialog.setTitle(title);
+        builder.setMessage(message)
+                .setTitle(title);
 
-        // Setting Dialog Message
-        alertDialog.setMessage(message);
+        builder.setIcon((status) ? R.drawable.success : R.drawable.fail);
 
-        // Setting alert dialog icon
-        alertDialog.setIcon((status) ? R.drawable.success : R.drawable.fail);
-
-        // Setting OK Button
-        alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int which) {
+        builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked OK button
+            }
+        });
+        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User cancelled the dialog
             }
         });
 
+        AlertDialog dialog = builder.create();
+
+
         // Showing Alert Message
-        alertDialog.show();
+        dialog.show();
     }
 
 }
